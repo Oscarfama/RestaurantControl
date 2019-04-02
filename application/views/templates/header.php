da5dc2e2fbb973d7e2ec751fbaef5d09f32a7de1
Prometheus <prometheus@polispay.org>
2019-04-01 18:55:47 -0600

    UPload data + log in front end Ready

---
+++ application/views/templates/header.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title> Restaurant Blog</title>
+		<link rel= "stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="home">Restaurant</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor03">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about">About</a>
+      </li>
+    </ul>
+    <!--
+    <form class="form-inline my-2 my-lg-0">      <input class="form-control mr-sm-2" type="text" placeholder="Search">     <button class="btn btn-secondarymy-2 my-sm-0" type="submit">Search</button>    </form>
+-->
+  </div>
+</nav>
