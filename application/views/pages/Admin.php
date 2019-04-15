80aea5412719c20126c5eed5b1102d0171923880
Prometheus <prometheus@polispay.org>
2019-04-15 16:11:36 -0500

    some calls to database

--- application/views/pages/Admin.php
+++ application/views/pages/Admin.php
@@ -1,28 +1,58 @@
-<?php
- include 'LogInPhp.php'
-?>
  <div class="row tours">
-        <div class="tourcard">
+ <?php
+ $host = "localhost";
+$user="root";
+$password = "";
+$db = "parcial2";
+$id= '';
+
+$conn = new mysqli($host,$user,$password, $db);
+if($conn -> connect_error){
+    die("Connection failed:". $conn -> connect_error);
+}
+$sql = "select Id_Restaurant, Nombre, Direccion from restaurant"; 
+$result = $conn->query($sql);
+
+if($result -> num_rows>0){
+    while ($row = $result-> fetch_assoc()){
+        ?>
+            <div class="tourcard">
             <figure>
                 <div class="tourpic">
                     <img width="320" height="180" src="https://placeimg.com/400/225/nature">
                 </div>
                 <figcaption>
                   <h3 class="entry-title">
-                      <a href="https://reezhdesign.com"><?php echo $values["email"]?> Empresa</a></h3>
-                  <span class="description">Direccion</span>
+                      <a href="https://reezhdesign.com"><?php echo $row["Nombre"] ?> </a></h3>
+                  <span class="description"><?php  echo $row["Direccion"]?></span>
                 </figcaption>
                 <div class="tourbtn">
-                    <a href="#" class="btn btn-sm" target="_blank" rel="noreferrer">
+                    <a class="btn btn-sm" >
                       <i class="fa fa-edit"></i><span>Edit</span>
                     </a>
-                    <a href="#" class="btn btn-sm" target="_blank" rel="noreferrer">
-                        <i class="fa fa-trash"></i><span>Delete</span>
+                    <a method ="POST" class="btn btn-sm" target="_blank" rel="noreferrer">
+                        <i class="fa fa-trash"  name="Delete" type= "Delete"></i><span>Delete</span>
                     </a>
                 </div>
             </figure>
         </div>
-    </div>
+        <?php 
+        echo  $row["Id_Restaurant"];
+    }
+     if (isset($_POST['Delete'])){
+        echo "dentro de accion del boton";
+            $sql = "delete from restaurant where Id_Restaurant = '".$row["Id_Restaurant"]."'"; 
+            $result = $conn->query($sql);
+            echo "np esta funcionandooo";
+        }
+
+}
+else { 
+echo "No Clients";
+}
+
+ ?>
+</div>
     
 <div style="text-align: center;">
 <a href="Form" class="btn btn-info" role="button" style="background-color: #4CAF50; /* Green */
--- application/views/pages/Form.php
+++ application/views/pages/Form.php
@@ -1,8 +1,28 @@
+<?php
+session_start();
+$host = "localhost";
+$user="root";
+$password = "";
+$db = "parcial2";
+$hola= '';
+
+$conn = new mysqli($host,$user,$password, $db);
+if(isset($_POST['name'])){
+	$name= $_POST['name'];
+	$dir=$_POST['direccion'];
+	$sql = "insert into restaurant (Nombre, Direccion) values('$name','$dir')"; 
+
+	if($conn->query($sql) ===TRUE){
+		header("location: admin");
+	}
+}
+?>
+<h2><?php $name ?></h2>
 <div class="container">
 	<div class="main">
 		<div class="main-center">
 			<h5>Restaurant Manager</h5>
-				<form class="" method="post" action="#">
+				<form action="" method="POST">
 					<div class="form-group">
 						<label for="name">Nombre del Restaurant</label>
 							<div class="input-group">
@@ -14,7 +34,7 @@
 						<label for="email">Direccion</label>
 							<div class="input-group">
 								<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-								<input type="text" class="form-control" name="email" placeholder="Enter your Email"/>
+								<input type="text" class="form-control" name="direccion" placeholder="Enter your Email"/>
 							</div>
 					</div>
 					<div style="text-align: center">
